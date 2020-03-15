a6f900c50505347053953d2897a5bfea62a288f0
Mayranush Nazaretyan <mayranush_nazaretyan@epam.com>
2020-03-15 19:18:23 +0400

    Task 2.2

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,11 @@
 const express = require('express');
+const Joi = require('@hapi/joi');
+import {
+    createValidator
+} from 'express-joi-validation';
+
 let router = express.Router();
+
 const {
     getUser,
     getAutoSuggestUsers,
@@ -8,11 +14,32 @@ const {
     deleteUser,
 } = require('../controllers/users');
 
+const validator = createValidator();
+const schema = Joi.object({
+    id: Joi.string().required()
+});
 
-router.get('/:id', getUser);
+const bodySchema = Joi.object({
+    id: Joi.string()
+        .required(),
+    login: Joi.string()
+        .required(),
+    password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]'))
+        .required(),
+    age: Joi.number()
+        .integer()
+        .min(4)
+        .max(130)
+        .required(),
+    isDeleted: Joi.boolean()
+        .required(),
+});
+
+router.get('/:id', validator.params(schema), getUser);
 router.get('/', getAutoSuggestUsers);
-router.post('/', createUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.post('/', validator.body(bodySchema), createUser);
+router.put('/:id', validator.params(schema), validator.body(bodySchema), updateUser);
+router.delete('/:id', validator.params(schema), deleteUser);
 
 module.exports = router;
